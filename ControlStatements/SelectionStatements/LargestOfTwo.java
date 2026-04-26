b5ae50e537e19c5384ab8551abe20893dc4ccd2b
Ritesh <riteshman30@gmail.com>
2026-04-26 12:13:29 +0530

    Add LargestOfTwo program to compare two integers

---
+++ ControlStatements/SelectionStatements/LargestOfTwo.java
@@ -0,0 +1,12 @@
+ class LargestOfTwo {
+    public static void main (String[] args){
+        int a=10;
+        int b=20;
+        if(b>a){
+            System.out.print("b is greater than a");
+        }
+        else{
+            System.out.print("a is greater than b");
+        }
+    }
+}
